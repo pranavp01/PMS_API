7f4748b8c9412e864e0efabdcf55a4166aaacf05
pranav peddi <pranavped@gmail.com>
2021-11-19 09:49:22 +0530

    Add project files.

---
+++ PMS_Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PMS_Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ PMS_Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PMS_Repository
+{
+    public class Class1
+    {
+    }
+}
